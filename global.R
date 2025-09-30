7c2bd33afe51b7b320641d76af7e056a77dbbc57
Anna Mathieu <annamathieupro@gmail.com>
2025-09-30 10:49:31 +0200

    LIBRARIES

---
+++ global.R
@@ -0,0 +1,12 @@
+
+# Librairies  
+
+library(tidyverse)          # %>% 
+library(dplyr)              # manipulation de données 
+library(tidyr)              # manipulation de tibbles ; opérations de join 
+library(sf)                 # cartes interactives et manipulation d'objets sf 
+library(rnaturalearth)      # fournit données géographiques
+library(leaflet)            # interactives maps
+library(rnaturalearthdata)
+library(ggplot2)            # pour faire des plots 
+library(RColorBrewer)       # palettes de couleur 
