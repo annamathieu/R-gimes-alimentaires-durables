4f688a184fe60efc185b02a6e585e78f80ef1694
Anna Mathieu <annamathieupro@gmail.com>
2025-09-30 22:00:11 +0200

    n

--- 03_a.R
+++ 03_a.R
@@ -1,2 +1,46 @@
 # Visualisations des régimes alimentaires et leurs impacts nutritionnels 
 
+# % de couverture des apports nutritionnels par les régimes alimentaires 
+# %rec , on va regarder que sur un pays à la fois 
+
+# jeu de données à utiliser : nutri_new 
+
+# chargement des librairies
+library(ggplot2)
+library(dplyr)
+library(tidyverse)
+library(tidyr)
+
+nutri_new <- as.data.frame(nutri_new)
+
+# ajout d'une colonne "grp_diet" de classe de type de scénario de diet 
+nutri_new <- nutri_new %>%
+  mutate(grp_diet = case_when(
+    diet.scenario %in% c("FLX", "PSC", "VEG", "VGN") ~ "vg",
+    diet.scenario %in% c("ani-25", "ani-50", "ani-75", "ani-100") ~ "ani", 
+    diet.scenario %in% c("kcal-25", "kcal-50", "kcal-75", "kcal-100") ~ "kcal", 
+    TRUE ~ "bmk"))
+
+# tous les régimes alimentaires simultanément 
+
+nutri_new %>% 
+  filter((code_pays=="FRP") & (nutri_new$item=="%rec")) %>% 
+  ggplot() +
+  aes(fct_reorder(diet.scenario, protein), protein, fill = grp_diet, group = grp_diet) +
+  geom_bar(stat= "identity", position = position_dodge())+
+  geom_text(aes(y = protein, 
+                label = round(protein,0),
+                group =grp_diet), 
+            color = "black", 
+            size = 4,
+            vjust = 1) +
+  labs(fill = "Groupe de \nscénario \nrégime \nalimentaire", 
+       title = "% de couverture des protéines par le régime alimentaire \npar rapport aux recommandations nutritionnelles") + 
+  theme(text = element_text(size = 10),
+        axis.text.x = element_text(angle = 45, hjust = 1),
+        title = element_text(size = 9)) +
+  xlab ("Régime alimentaire") 
+  
+
+
+# un régime à la fois 
