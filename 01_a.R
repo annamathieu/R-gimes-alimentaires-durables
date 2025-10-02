5e00ef6703211eec7078d68b7bdc67206c97b5b0
Nassima EL HILALI <nassima.elhilali.pro@gmail.com>
2025-10-02 16:59:12 +0200

    env_new

--- 01_a.R
+++ 01_a.R
@@ -51,6 +51,14 @@ env_new <- env_new[-ends_with( "LMIC", vars = env_new$region) , ] # idem
 env_new <- rename(env_new,code_pays=region)                       # renomme code pays 
 env_new <- pivot_wider(data = env_new, names_from = environmental.domain, values_from = value)
 
+env_new <- env_new %>%
+  mutate(grp_diet = case_when(
+    diet.scenario %in% c("FLX", "PSC", "VEG", "VGN") ~ "vg",
+    diet.scenario %in% c("ani-25", "ani-50", "ani-75", "ani-100") ~ "ani", 
+    diet.scenario %in% c("kcal-25", "kcal-50", "kcal-75", "kcal-100") ~ "kcal", 
+    TRUE ~ "bmk"))
+
+
 write.csv(env_new, file="data_csv/env_new.csv")
 
 
