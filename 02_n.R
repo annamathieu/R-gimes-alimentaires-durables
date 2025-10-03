ea8edecdb7f6d2bf2f073569ab59a08134717c1c
Nassima EL HILALI <nassima.elhilali.pro@gmail.com>
2025-10-03 10:51:29 +0200

    w

--- 02_n.R
+++ 02_n.R
@@ -1,10 +1,32 @@
 
 
-head(env_new)
-env_new <- env_new %>%
-  mutate(grp_diet = case_when(
-    diet.scenario %in% c("FLX", "PSC", "VEG", "VGN") ~ "vg",
-    diet.scenario %in% c("ani-25", "ani-50", "ani-75", "ani-100") ~ "ani", 
-    diet.scenario %in% c("kcal-25", "kcal-50", "kcal-75", "kcal-100") ~ "kcal", 
-    TRUE ~ "bmk"))
+summary(env_new)
 
+# tous les régimes alimentaires simultanément POUR 
+
+all.diet <- function(nutrim = "protein") {
+  
+  graph <- nutri_new %>% 
+    filter((code_pays=="FRP") & (nutri_new$item=="%rec")) %>% 
+    ggplot() +
+    aes(fct_reorder(diet.scenario, .data[[nutrim]]), .data[[nutrim]], fill = grp_diet, group = grp_diet) +
+    geom_bar(stat= "identity", position = position_dodge())+
+    geom_text(aes(y = .data[[nutrim]], 
+                  label = round(.data[[nutrim]],0),
+                  group =grp_diet), 
+              color = "black", 
+              size = 4,
+              vjust = 1) +
+    labs(fill = "Groupe de \nscénario \nrégime \nalimentaire", 
+         title = "% de couverture des protéines par le régime alimentaire \npar rapport aux recommandations nutritionnelles") + 
+    theme(text = element_text(size = 10),
+          axis.text.x = element_text(angle = 45, hjust = 1),
+          title = element_text(size = 9)) +
+    xlab ("Régime alimentaire") +
+    geom_hline(yintercept = 100, 
+               linetype = "dashed", 
+               color = "black", 
+               linewidth = 0.8)
+  
+  return(graph )
+}
