0d0ee88cf24d55dad59821681809cef50d80f6a4
Anna Mathieu <annamathieupro@gmail.com>
2025-10-06 18:52:56 +0200

    glbal

--- R-gimes-alimentaires-durables/global.R
+++ R-gimes-alimentaires-durables/global.R
@@ -8,12 +8,25 @@ library(sf)                 # cartes interactives et manipulation d'objets sf
 library(rnaturalearth)      # fournit données géographiques
 library(leaflet)            # interactives maps
 library(rnaturalearthdata)
+
 library(ggplot2)            # pour faire des plots 
 library(RColorBrewer)       # palettes de couleur 
+library(patchwork)          # pour afficher des patchwork de graph 
+library(purrr)
 
+library(FactoMineR)         # analyses factorielles 
+library(factoextra)         # personnalisation de graphiques d'analyse factorielle 
 
+library(DT)                 # Data tables 
 
-# couleurs des diets 
+world <- ne_countries(scale = "medium", returnclass = "sf")
+world$region_un <- as.factor(world$region_un)
+
+nutri_new <- read.table(file = "data_csv/nutri_new.csv", header = T, sep =",", stringsAsFactors = T, dec = ".")
+sante_new <- read.table(file = "data_csv/sante_new.csv", header = T, sep =",", stringsAsFactors = T, dec = ".")
+env_new <- read.table(file = "data_csv/env_new.csv", header = T, sep =",", stringsAsFactors = T, dec = ".")
+conti<- read.table(file = "data_csv/conti.csv", header = T, sep=",", stringsAsFactors = T)
+
 colors.scenario <- c("mistyrose",  # ani-100
                      "brown4",        # ani 25
                      "indianred3",    # ani 50
@@ -34,34 +47,4 @@ names(colors.scenario) = c("ani-100", "ani-25", "ani-50", "ani-75",
                            "kcal-100", "kcal-25", 
                            "kcal-50" , "kcal-75", 
                            "PSC", "VEG", "VGN") 
-
-
-
-# save data
-# if (!dir.exists("R-gimes-alimentaires-durables/data")) {
-#   dir.create("R-gimes-alimentaires-durables/data")
-# }
-# saveRDS(nutri_new, "R-gimes-alimentaires-durables/data/nutri_new.rds")
-# saveRDS(sante_new, "R-gimes-alimentaires-durables/data/sante_new.rds")
-# saveRDS(env_new, "R-gimes-alimentaires-durables/data/env_new.rds")
-# saveRDS(conti, "R-gimes-alimentaires-durables/data/conti.rds")
-
-
-# import data
-nutri_new <- readRDS(file = "data/nutri_new.rds")
-sante_new <- readRDS(file = "data/sante_new.rds")
-env_new <- readRDS(file = "data/env_new.rds")
-conti<- readRDS(file = "data/conti.rds")
-
-
-
-# variables globales
-
-world <- ne_countries(scale = "medium", returnclass = "sf")
-world$region_un <- as.factor(world$region_un)
-
-# Charger les fonctions le dossier RScripts
-source("RScripts/carto.R")
-
-
 
